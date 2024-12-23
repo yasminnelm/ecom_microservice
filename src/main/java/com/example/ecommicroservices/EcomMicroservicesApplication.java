71f5b39a28f62e7a11e7746eca04ad9f2860e800
yasminnelm <yassamineelm11@gmail.com>
2024-12-24 00:31:41 +0100

    Initial commit

---
+++ src/main/java/com/example/ecommicroservices/EcomMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomMicroservicesApplication.class, args);
+    }
+
+}
